ffa18f48858532a7f5282e92f435dc0619b5f9b9
Wang HengXing <35129760@qq.com>
2026-09-23 09:12:05 +0800

    [+] 补充前缀树

--- src/gee/trie.go
+++ src/gee/trie.go
@@ -39,18 +39,11 @@ func (n *node) insert(pattern string, parts []string, height int) {
 	}
 
 	part := parts[height]
-
 	child := n.matchChild(part)
-
-	if child != nil {
-		child = &node{
-			part:   part,
-			isWild: part[0] == ':' || part[0] == '*',
-		}
-
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
-
 	child.insert(pattern, parts, height+1)
 }
 
@@ -67,7 +60,6 @@ func (n *node) search(parts []string, height int) *node {
 
 	for _, child := range children {
 		result := child.search(parts, height+1)
-
 		if result != nil {
 			return result
 		}
